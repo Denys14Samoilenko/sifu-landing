ed956fc5b979817453429b1114f60a1437a7fab8
Denys <d.samoilenko92@gmail.com>
2024-09-18 17:43:20 +0300

    feat: add banner section

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 const toggle = document.getElementsByClassName('navbar-toggle');
 toggle[0].addEventListener('click', function (event) {
-  event.preventDefault();
-  let scrollMenu = toggle[0].classList.toggle('is-active');
-  if (scrollMenu === true) {
-    document.body.style.overflow = 'hidden';
-  } else {
-    document.body.style.overflow = 'visible';
-  }
+	event.preventDefault();
+	let scrollMenu = toggle[0].classList.toggle('is-active');
+	if (scrollMenu === true) {
+		document.body.style.overflow = 'hidden';
+	} else {
+		document.body.style.overflow = 'visible';
+	}
 });
+
+const swiper = new Swiper('.banner-swiper', {
+	pagination: {
+		el: '.swiper-pagination',
+	},
+});
+
+// navigation: {
+// 	nextEl: '.swiper-button-next',
+// 	prevEl: '.swiper-button-prev',
+// },
